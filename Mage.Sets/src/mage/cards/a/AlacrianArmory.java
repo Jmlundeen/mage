5b178be70057ab744f6c75e076c7374d22d62cd1
jmlundeen <jmlundee@yahoo.com>
2025-03-04 20:16:23 -0600

    update Alacrian Armory

--- Mage.Sets/src/mage/cards/a/AlacrianArmory.java
+++ Mage.Sets/src/mage/cards/a/AlacrianArmory.java
@@ -3,15 +3,12 @@ package mage.cards.a;
 import java.util.UUID;
 
 import mage.abilities.Ability;
-import mage.abilities.common.CrewSaddleIncreasedPowerAbility;
 import mage.abilities.common.SimpleStaticAbility;
 import mage.abilities.effects.Effect;
 import mage.abilities.effects.OneShotEffect;
-import mage.abilities.effects.common.EndTurnEffect;
 import mage.abilities.effects.common.continuous.AddCardTypeTargetEffect;
 import mage.abilities.effects.common.continuous.BoostControlledEffect;
 import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
-import mage.abilities.keyword.CrewAbility;
 import mage.abilities.keyword.SaddleAbility;
 import mage.abilities.keyword.VigilanceAbility;
 import mage.abilities.triggers.BeginningOfCombatTriggeredAbility;
@@ -25,12 +22,9 @@ import mage.constants.TargetController;
 import mage.filter.FilterPermanent;
 import mage.filter.common.FilterCreaturePermanent;
 import mage.filter.predicate.Predicates;
-import mage.filter.predicate.permanent.SaddledSourceThisTurnPredicate;
 import mage.game.Game;
-import mage.game.events.GameEvent;
 import mage.game.permanent.Permanent;
 import mage.target.TargetPermanent;
-import mage.target.targetpointer.FixedTarget;
 
 /**
  *
