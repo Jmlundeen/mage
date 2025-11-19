c9ab99ff8313f9c0816a030febf24f754556e214
jmlundeen <jmlundee@yahoo.com>
2025-11-19 13:44:42 -0600

    ZonesHandler - move position of setting face down status

--- Mage/src/main/java/mage/game/ZonesHandler.java
+++ Mage/src/main/java/mage/game/ZonesHandler.java
@@ -149,10 +149,6 @@ public final class ZonesHandler {
             targetCard.setZone(toZone, game);
             return;
         }
-        if (!info.faceDown) {
-            // make sure card is face up if not specifically set to move face down
-            targetCard.setFaceDown(false);
-        }
 
         Cards cardsToMove = null; // moving real cards
         Map<Zone, Cards> cardsToUpdate = new LinkedHashMap<>(); // updating all card's parts (must be ordered LinkedHashMap)
@@ -315,6 +311,9 @@ public final class ZonesHandler {
                 }
             }
         });
+
+        // set face down status
+        targetCard.setFaceDown(info.faceDown);
 
         // reset meld status
         if (targetCard instanceof MeldCard) {
