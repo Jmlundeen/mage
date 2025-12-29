93a226b7e61c8eeae29edf65e5740d68d373e50a
jmlundeen <jmlundee@yahoo.com>
2025-12-29 11:10:50 -0600

    Relic's Roar - update use of BecomesCreatureTargetEffect

--- Mage.Sets/src/mage/cards/r/RelicsRoar.java
+++ Mage.Sets/src/mage/cards/r/RelicsRoar.java
@@ -1,11 +1,13 @@
 package mage.cards.r;
 
-import mage.abilities.effects.common.continuous.BecomesCreatureTargetEffect;
+import mage.abilities.effects.common.continuous.generic.ContinuousEffectBuilder;
 import mage.cards.CardImpl;
 import mage.cards.CardSetInfo;
-import mage.constants.*;
+import mage.constants.CardType;
+import mage.constants.Duration;
+import mage.constants.Outcome;
+import mage.constants.SubType;
 import mage.filter.StaticFilters;
-import mage.game.permanent.token.custom.CreatureToken;
 import mage.target.TargetPermanent;
 
 import java.util.UUID;
@@ -19,10 +21,11 @@ public final class RelicsRoar extends CardImpl {
         super(ownerId, setInfo, new CardType[]{CardType.INSTANT}, "{U}");
 
         // Until end of turn, target artifact or creature becomes a Dinosaur artifact creature with base power and toughness 4/3 in addition to its other types.
-        this.getSpellAbility().addEffect(new BecomesCreatureTargetEffect(
-                new CreatureToken(4, 3, "Dinosaur artifact creature with base power and toughness 4/3 in addition to its other types"),
-                false, false, Duration.EndOfTurn)
-                .withDurationRuleAtStart(true)
+        this.getSpellAbility().addEffect(
+                new ContinuousEffectBuilder(Duration.EndOfTurn, Outcome.BecomeCreature)
+                        .withAddedCardTypes(CardType.CREATURE, CardType.ARTIFACT)
+                        .withAddedSubTypes(false, SubType.DINOSAUR)
+                        .withSetPowerAndToughness(4, 3)
         );
         this.getSpellAbility().addTarget(new TargetPermanent(StaticFilters.FILTER_PERMANENT_ARTIFACT_OR_CREATURE));
     }
